e163bce35c7ed958bcd40e82ddacb1e31ceee8f0
seungboshim-iconx <sb.shim@iconx.co.kr>
2025-04-08 13:44:05 +0900

    fix: fix lint build error

--- src/components/layout/MainLayout/types.ts
+++ src/components/layout/MainLayout/types.ts
@@ -1,19 +1,19 @@
 import { HTMLAttributes, ReactNode } from 'react';
-import { Rule } from '@/lib/markdown-utils';
+import { Rule } from '@/types/rule';
 
 export interface MainLayoutProps extends HTMLAttributes<HTMLDivElement> {
   /**
    * 레이아웃 내부 콘텐츠
    */
   children: ReactNode;
-  
+
   /**
    * 규칙 목록
    */
   rules: Rule[];
-  
+
   /**
    * 현재 선택된 규칙 ID
    */
   currentRuleId?: string;
-} 
+}
